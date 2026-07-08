b1c04050148571c54346951b4775d8bc847f20a6
Ayeshaanjum2904 <ayeshaanjum2904@gmail.com>
2026-07-08 10:56:50 +0530

    security(sast): automatically remediate 2 infrastructure issue(s) in clientSideFiltering.js

--- src/main/resources/lessons/clientsidefiltering/js/clientSideFiltering.js
+++ src/main/resources/lessons/clientsidefiltering/js/clientSideFiltering.js
@@ -3,7 +3,13 @@ var dataFetched = false;
 function selectUser() {
 
     var newEmployeeID = $("#UserSelect").val();
-    document.getElementById("employeeRecord").innerHTML = document.getElementById(newEmployeeID).innerHTML;
+    var employeeRecordElement = document.getElementById(newEmployeeID);
+    if (employeeRecordElement) {
+        var sanitizedContent = document.createTextNode(employeeRecordElement.innerHTML);
+        var container = document.getElementById("employeeRecord");
+        container.textContent = ""; // Clear existing content
+        container.appendChild(sanitizedContent);
+    }
 }
 
 function fetchUserData() {
@@ -14,29 +20,35 @@ function fetchUserData() {
 }
 
 function ajaxFunction(userId) {
-    $.get("clientSideFiltering/salaries?userId=" + userId, function (result, status) {
-        var html = "<table border = '1' width = '90%' align = 'center'";
-        html = html + '<tr>';
-        html = html + '<td>UserID</td>';
-        html = html + '<td>First Name</td>';
-        html = html + '<td>Last Name</td>';
-        html = html + '<td>SSN</td>';
-        html = html + '<td>Salary</td>';
+    $.get("clientSideFiltering/salaries?userId=" + encodeURIComponent(userId), function (result, status) {
+        var html = "<table border = '1' width = '90%' align = 'center'>";
+        html += '<tr>';
+        html += '<td>UserID</td>';
+        html += '<td>First Name</td>';
+        html += '<td>Last Name</td>';
+        html += '<td>SSN</td>';
+        html += '<td>Salary</td>';
 
         for (var i = 0; i < result.length; i++) {
-            html = html + '<tr id = "' + result[i].UserID + '"</tr>';
-            html = html + '<td>' + result[i].UserID + '</td>';
-            html = html + '<td>' + result[i].FirstName + '</td>';
-            html = html + '<td>' + result[i].LastName + '</td>';
-            html = html + '<td>' + result[i].SSN + '</td>';
-            html = html + '<td>' + result[i].Salary + '</td>';
-            html = html + '</tr>';
+            html += '<tr id = "' + encodeURIComponent(result[i].UserID) + '">';
+            html += '<td>' + escapeHtml(result[i].UserID) + '</td>';
+            html += '<td>' + escapeHtml(result[i].FirstName) + '</td>';
+            html += '<td>' + escapeHtml(result[i].LastName) + '</td>';
+            html += '<td>' + escapeHtml(result[i].SSN) + '</td>';
+            html += '<td>' + escapeHtml(result[i].Salary) + '</td>';
+            html += '</tr>';
         }
-        html = html + '</tr></table>';
+        html += '</table>';
 
         var newdiv = document.createElement("div");
         newdiv.innerHTML = html;
         var container = document.getElementById("hiddenEmployeeRecords");
         container.appendChild(newdiv);
     });
+}
+
+function escapeHtml(string) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(string));
+    return div.innerHTML;
 }
