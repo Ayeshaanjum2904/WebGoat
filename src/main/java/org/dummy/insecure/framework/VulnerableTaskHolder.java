7fb3b1d1f850d8368cedf8babfdacefbe0eb6cdf
Ayeshaanjum2904 <ayeshaanjum2904@gmail.com>
2026-07-09 17:01:36 +0530

    security(sast): automatically remediate 1 infrastructure issue(s) in VulnerableTaskHolder.java

--- src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
+++ src/main/java/org/dummy/insecure/framework/VulnerableTaskHolder.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -64,6 +65,7 @@ public class VulnerableTaskHolder implements Serializable {
         && taskAction.length() < 22) {
       log.info("about to execute: {}", taskAction);
       try {
+        validateTaskAction(taskAction);
         Process p = Runtime.getRuntime().exec(taskAction);
         BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line = null;
@@ -73,6 +75,13 @@ public class VulnerableTaskHolder implements Serializable {
       } catch (IOException e) {
         log.error("IO Exception", e);
       }
+    }
+  }
+
+  private void validateTaskAction(String taskAction) {
+    String allowedPattern = "^(sleep|ping)(\s+\S+)*$";
+    if (!Pattern.matches(allowedPattern, taskAction)) {
+      throw new IllegalArgumentException("Invalid task action");
     }
   }
 }
